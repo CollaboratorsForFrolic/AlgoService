4fd6b41ba533fe555f24de937cd131980eec9793
Anshul Dev Mehta <anshul.dev.mehta@gmail.com>
2022-07-03 19:18:32 +0530

    First Commit

---
+++ src/main/java/com/microalgo/algoservice/AlgoServiceApplication.java
@@ -0,0 +1,18 @@
+package com.microalgo.algoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class AlgoServiceApplication {
+
+	public static Logger logger=Logger.getLogger("AlgoService");
+	public static void main(String[] args) {
+
+		SpringApplication.run(AlgoServiceApplication.class, args);
+		logger.info("Algo Service Started");
+	}
+
+}
---
+++ src/test/java/com/microalgo/algoservice/AlgoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microalgo.algoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
