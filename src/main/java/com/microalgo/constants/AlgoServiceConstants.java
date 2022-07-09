ba9bb12ddfb69982509a0d2d04d3194f5cf3db82
Anshul Dev Mehta <anshul.dev.mehta@gmail.com>
2022-07-09 21:42:24 +0530

    Added constant for localhost

--- src/main/java/com/microalgo/constants/AlgoServiceConstants.java
+++ src/main/java/com/microalgo/constants/AlgoServiceConstants.java
@@ -6,5 +6,6 @@ public final class AlgoServiceConstants
     public static class URIConstants
     {
         public static final String STRING_PING_END_POINT="/stringping";
+        public static final String LOCAL_HOST="http://localhost:";
     }
 }
--- src/test/java/com/microalgo/integrationtests/StringControllerIntegrationTest.java
+++ src/test/java/com/microalgo/integrationtests/StringControllerIntegrationTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import static com.microalgo.constants.AlgoServiceConstants.URIConstants.LOCAL_HOST;
 import static com.microalgo.constants.AlgoServiceConstants.URIConstants.STRING_PING_END_POINT;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -30,7 +31,7 @@ public class StringControllerIntegrationTest
 
         //act
         HttpStatus actualStatusCode=this.restTemplate
-                        .getForEntity("http://localhost:" + port + STRING_PING_END_POINT, ResponseEntity.class).getStatusCode();
+                        .getForEntity(LOCAL_HOST+ port + STRING_PING_END_POINT, ResponseEntity.class).getStatusCode();
 
         assertEquals(HttpStatus.OK,actualStatusCode);
     }
