fda63d72aeb083a018c03b2d1d93a22ff66f394a
pratimsh <1708sharma@gmail.com>
2022-07-13 23:41:07 +0530

    Second Commit , Optimization of core logic using StringBuilder and added multiple tests

--- src/main/java/com/microalgo/service/StringRunLengthEncodingServices.java
+++ src/main/java/com/microalgo/service/StringRunLengthEncodingServices.java
@@ -1,37 +1,43 @@
 package com.microalgo.service;
 
+import io.micrometer.core.instrument.util.StringUtils;
+
 public class StringRunLengthEncodingServices {
     public String runLengthEncoding(String input){
+
+            if(StringUtils.isBlank(input) || StringUtils.isEmpty(input)){
+                return "Un-necessary operation, String is empty or null";
+            }
             int size = input.length();
             char[] charArray = input.toCharArray();
             char temp = charArray[0];
             int count = 1;
-            String output = "";
+            StringBuilder output = new StringBuilder();
             for(int i = 1; i <size; i++){
 
                 if(charArray[i] == temp){
                     count++;
                 }else if (charArray[i] != temp){
-                    if(count > 1) {
-                        output = output + temp + String.valueOf(count);
-
-                    }else{
-                        output = output + temp;
-                    }
+                    encodeTheValues( count, output, temp);
                     count = 1;
                     temp = charArray[i];
                 }
 
             }
-
-            if(count > 1) {
-                output = output + temp + String.valueOf(count);
-
-            }else{
-                output = output + temp;
-            }
-
+            encodeTheValues( count, output, temp);
             System.out.println(output);
-            return output;
+            return String.valueOf(output).replaceAll("\\s+", "");
     }
+
+    private StringBuilder encodeTheValues(int count,StringBuilder output,char temp){
+
+        if(count > 1) {
+            output = output.append(temp).append(count);
+
+        }else{
+            output = output.append(temp);
+        }
+        return output;
+    }
+
 }
--- src/test/java/com/microalgo/service/StringRunLengthEncodingServicesTest.java
+++ src/test/java/com/microalgo/service/StringRunLengthEncodingServicesTest.java
@@ -6,13 +6,72 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringRunLengthEncodingServicesTest {
 
+    StringRunLengthEncodingServices stringRunLengthEncodingServices = new StringRunLengthEncodingServices();
+
+
+    //Trying a vanilla scenario
     @Test
-    void runLengthEncodingTest(){
+    void runLengthEncodingVanillaTest(){
         //assume
         String input = "aaabbcddddeeaabbb";
         String output = "a3b2cd4e2a2b3";
         //assemble
-        StringRunLengthEncodingServices stringRunLengthEncodingServices = new StringRunLengthEncodingServices();
+
+        //act
+        String response = stringRunLengthEncodingServices.runLengthEncoding(input);
+        //assert
+        assertEquals(output,response);
+    }
+
+    //trying an empty string
+    @Test
+    void runLengthEncodingEmptyTest(){
+        //assume
+
+        //assemble
+
+        //act
+        String response = stringRunLengthEncodingServices.runLengthEncoding("");
+        //assert
+        assertEquals("Un-necessary operation, String is empty or null",response);
+    }
+
+
+    //trying a null
+    @Test
+    void runLengthEncodingNullTest(){
+        //assume
+
+        //assemble
+
+        //act
+        String response = stringRunLengthEncodingServices.runLengthEncoding(null);
+        //assert
+        assertEquals("Un-necessary operation, String is empty or null",response);
+    }
+
+    //Trying a vanilla scenario with numbers
+    @Test
+    void runLengthEncodingWithNumericStringTest(){
+        //assume
+        String input = "111223333";
+        String output = "132234";
+        //assemble
+
+        //act
+        String response = stringRunLengthEncodingServices.runLengthEncoding(input);
+        //assert
+        assertEquals(output,response);
+    }
+
+    //Trying a scenario where the string containes spaces
+    @Test
+    void runLengthEncodingWithSpacesTest(){
+        //assume
+        String input = "aaabbc dddd eeaabbb";
+        String output = "a3b2cd4e2a2b3";
+        //assemble
+
         //act
         String response = stringRunLengthEncodingServices.runLengthEncoding(input);
         //assert
